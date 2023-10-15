151ad9a89ee6685f7bf0d4b183b820dcce23b42c
Hao Nguyen <hao.nguyen@iths.se>
2023-10-15 11:21:22 +0200

    ArgumentException

--- Car Rental.Common/Exceptions/InputException.cs
+++ Car Rental.Common/Exceptions/InputException.cs
@@ -1,6 +1,6 @@
 ﻿namespace Car_Rental.Common.Exceptions;
 
-public class InputException : Exception
+public class InputException : ArgumentException
 {
     public InputException(string message) : base(message)
     {
